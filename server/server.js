0f1940b842f38a237adb7f0b5ca96933222cc67d
sekema954 <sekema954@gmail.com>
2024-09-12 22:48:54 -0400

    initial commit

--- server/server.js
+++ server/server.js
@@ -1,19 +1,15 @@
 const express = require('express');
 const path = require('path');
-const app = express();
-const port = process.env.PORT || 5000;
 
-// Serve static files from the Vite build directory
+const app = express();
+const port = process.env.PORT || 3001;
+
+// Serve static files from the "dist" directory
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-// Handle GET requests to the root
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
-});
-
-// For any other routes, redirect to the Vite app
+// Handle all other routes by sending the index.html file
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
 app.listen(port, () => {
